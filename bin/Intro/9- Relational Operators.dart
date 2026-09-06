92537ccbd1fdf7df84d879bb12ef2fd9f3052967
ahmedtamer2006 <ahmedtamerfoc2000@gmail.com>
2026-09-07 00:15:38 +0300

    feat: Enhanced to show in debug console What is a and b before any proccess

--- bin/Intro/9- Relational Operators.dart
+++ bin/Intro/9- Relational Operators.dart
@@ -1,6 +1,8 @@
 void main() {
   double a = 10.7;
   double b = 10.3;
+  print(a.toInt());
+  print(b.toInt());
   print(a.toInt() == b.round()); // Return T/F
   print(a.toInt() > b.round()); // Return T/F
   print(a.toInt() < b.round()); // Return T/F
