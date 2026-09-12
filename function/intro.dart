810b641b8c8a530ed515072c28a4440cfd0fc851
ahmedtamer2006 <ahmedtamerfoc2000@gmail.com>
2026-09-12 15:14:41 +0300

    feat: Improve user prompts and error messages in area calculation

--- function/intro.dart
+++ function/intro.dart
@@ -3,20 +3,24 @@ import 'dart:io';
 void main() {
   double? w;
   double? h;
+  String name = "Ahmed";
+
   do {
-    print("Enter Width : ");
+    print("Enter Width Please : ");
     w = double.tryParse(stdin.readLineSync()!);
-    print("Enter Hight : ");
+
+    print("Enter Hight Please : ");
     h = double.tryParse(stdin.readLineSync()!);
+
     if (w == null || h == null) {
-      print("Please enter a valid number");
+      print("Please, $name Enter a Valid Number");
     }
   } while (w == null || h == null);
 
   area(w, h);
 }
 
-void area(double w, double h) {
+void area(w, h) {
   double c = w * h;
-  print("Result is : ${c.toInt()}");
+  print(c.toInt());
 }
