eb08e652e10f8f789f67465f057fa9ab99a08464
ahmedtamer2006 <ahmedtamerfoc2000@gmail.com>
2026-09-25 01:41:08 +0300

    Fix formatting and add comments in Inheritance_of_constructor.dart; update Employee constructor for clarity

--- bin/oop/inheritance_of_constructor.dart
+++ bin/oop/inheritance_of_constructor.dart
@@ -1,15 +1,17 @@
 void main() {
   Employee employee = Employee("Ahmed");
+  employee.name = "Ahmed";
 }
 
 class Users {
   String? name;
+  //Constructor:
   Users({this.name});
 }
 
 class Employee extends Users {
   // Super to inheret from constructor in the first class
-  Employee( String? name) :super(name : name) {
+  Employee(String? name) : super(name: name) {
     print("=> $name");
   }
 }
